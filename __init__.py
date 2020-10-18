1e5410c55dd01d10058d4a86b42c2eb87bd11f7f
yorickreum <webdesign@yorickreum.de>
2020-10-18 04:30:44 +0200

    Added functionality

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 import fnmatch
 import os
 
-from modules import cbpi
+from modules import cbpi, app
 from modules.core.controller import KettleController
 from modules.core.hardware import ActorBase
 from modules.core.props import Property
@@ -15,14 +15,16 @@ GPIO_LOW = '0'
 GPIO_IN = 'in'
 GPIO_OUT = 'out'
 
+POWER_VALS = [0, 33, 67, 100]
 
-class NotificationAPI(object):
-    def notify(self, headline, message, type="success", timeout=5000):
-        self.api.notify(headline, message, type, timeout)
+
+def closest_power(power):
+    return min(POWER_VALS, key=lambda x: abs(x - power))
 
 
 def listGPIO():
     # Lists any GPIO in found in /sys/class/gpio
+    app.logger.info("Listing GPIOs for TriplePower...")
     try:
         arr = []
         for dirname in os.listdir(GPIO_PATH):
@@ -41,6 +43,7 @@ def setupGPIO(device, value):
     # Sets up GPIO if not defined on system boot (i.e. /etc/rc.local)
     # echo  <gpio#> > /sys/class/gpio/export
     # echo <in/out> > /sys/class/gpio/gpio<#>/direction
+    app.logger.info("Seting up GPIO for TriplePower...")
     try:
         if not os.path.exists(GPIO_PATH + ('/gpio%d' % device)):
             with open(GPIO_PATH + '/export', 'w') as fp:
@@ -75,38 +78,46 @@ class TriplePower(ActorBase):
                             description="GPIO to which the actor's heater #3 is connected"
                             )
 
+    gpios = None
+    power_phases = None
+    power = 100  # 0, 33, 67, 100
+
     def init(self):
-        setupGPIO(int(self.gpio1), GPIO_OUT)
-        setupGPIO(int(self.gpio2), GPIO_OUT)
-        setupGPIO(int(self.gpio3), GPIO_OUT)
-        self.switch_gpio(self.gpio1, False)
-        self.switch_gpio(self.gpio2, False)
-        self.switch_gpio(self.gpio3, False)
+        self.gpios = [self.gpio1, self.gpio2, self.gpio3]
+        self.power_phases = {
+            0: [],
+            33: [self.gpio1],
+            67: [self.gpio1, self.gpio2],
+            100: self.gpios,
+        }
+        for gpio in self.gpios:
+            setupGPIO(int(gpio), GPIO_OUT)
+            self.switch_gpio(gpio, False)
 
     def on(self, power=100):
-        cbpi.app.logger.info("TriplePower on got power: %s" % str(power))
-        self.set_power(power)
-        # else:
-        #     raise ValueError("Power was none of 0, 33, 66, 99 or 100! Switching all heaters off...")
+        app.logger.info(
+            "TriplePower on called with power %s and self.power set to %s." % (str(power), str(self.power))
+        )
+        self.switch_gpios(power)
 
     def set_power(self, power):
-        cbpi.app.logger.info("TriplePower set power got power: %s" % str(power))
-        if power is 0:
-            self.switch_gpio(self.gpio1, False)
-            self.switch_gpio(self.gpio2, False)
-            self.switch_gpio(self.gpio3, False)
-        elif power == 33:
-            self.switch_gpio(self.gpio1, True)
-            self.switch_gpio(self.gpio2, False)
-            self.switch_gpio(self.gpio3, False)
-        elif power == 66:
-            self.switch_gpio(self.gpio1, True)
-            self.switch_gpio(self.gpio2, True)
-            self.switch_gpio(self.gpio3, False)
-        elif power is None or power == 99 or power == 100:
-            self.switch_gpio(self.gpio1, True)
-            self.switch_gpio(self.gpio2, True)
-            self.switch_gpio(self.gpio3, True)
+        app.logger.info("TriplePower set power got power: %s" % str(power))
+        if power is None:
+            power = 0
+        new_power = closest_power(power)
+        app.logger.info("TriplePower parsed power to: %s" % str(new_power))
+        if self.power != new_power:
+            active_gpios = self.power_phases[new_power]
+            self.api.notify("TriplePower",
+                            "Heater's power was set to %s %%, will use %s phases from now on." % (
+                                str(new_power), str(len(active_gpios))),
+                            type="info",  # maybe use success?
+                            timeout=7500
+                            )
+        self.power = new_power
+        if self.state == 1:
+            self.switch_gpios(power)
+        return self.power
 
     def switch_gpio(self, gpio, switchOn=False):
         if switchOn:
@@ -120,8 +131,25 @@ class TriplePower(ActorBase):
             else:  # self.active == "Low"
                 outputGPIO(int(gpio), GPIO_HIGH)
 
+    def switch_gpios(self, power):
+        if power is None:
+            power = self.power
+        else:
+            power = closest_power(power)
+        active_gpios = self.power_phases[power]
+        app.logger.info("TriplePower switches on %s phases..." % (str(len(active_gpios))))
+        for gpio in self.gpios:
+            self.switch_gpio(gpio, (gpio in active_gpios))
+
     def off(self):
-        self.set_power(0)
+        app.logger.info("TriplePower switches off all three phases...")
+        self.switch_gpio(self.gpio1, False)
+        self.switch_gpio(self.gpio2, False)
+        self.switch_gpio(self.gpio3, False)
+        self.api.notify("TriplePower",
+                        "Heater switched off all three phases.",
+                        type="warning",
+                        timeout=10000)
 
 
 @cbpi.controller
@@ -141,13 +169,20 @@ class TripleHysteresis(KettleController):
     phase_3_off = Property.Number("Offset Phase #3 Off", True, 0,
                                   description="Offset below target temp when heater should switch off phase three.")
 
+    @cbpi.try_catch(None)
+    def heater_set_power(self, power=100):
+        k = self.api.cache.get("kettle").get(self.kettle_id)
+        if k.heater is not None:
+            self.actor_power(power, int(k.heater))
+
     def stop(self):
-        '''
+        """
         Invoked when the automatic is stopped.
         Normally you switch off the actors and clean up everything
         :return: None
-        '''
+        """
         super(KettleController, self).stop()
+        self.heater_set_power(0)
         self.heater_off()
 
     def run(self):
@@ -158,17 +193,21 @@ class TripleHysteresis(KettleController):
         while self.is_running():
             offset = (self.get_target_temp() - self.get_temp())
 
-            should_phase1_be_on = (offset > float(self.phase_1_on)) and not (offset < float(self.phase_1_off))
-            should_phase2_be_on = (offset > float(self.phase_2_on)) and not (offset < float(self.phase_2_off))
-            should_phase3_be_on = (offset > float(self.phase_3_on)) and not (offset < float(self.phase_3_off))
+            should_phase1_be_on = (offset >= float(self.phase_1_on)) and not (offset <= float(self.phase_1_off))
+            should_phase2_be_on = (offset >= float(self.phase_2_on)) and not (offset <= float(self.phase_2_off))
+            should_phase3_be_on = (offset >= float(self.phase_3_on)) and not (offset <= float(self.phase_3_off))
 
             num_of_phases = int(should_phase1_be_on) + int(should_phase2_be_on) + int(should_phase3_be_on)
             if num_of_phases == 3:
+                self.heater_set_power(100)
                 self.heater_on(100)
             elif num_of_phases == 2:
+                self.heater_set_power(66)
                 self.heater_on(66)
             elif num_of_phases == 1:
+                self.heater_set_power(33)
                 self.heater_on(33)
             else:
+                self.heater_set_power(0)
                 self.heater_off()
             self.sleep(1)
